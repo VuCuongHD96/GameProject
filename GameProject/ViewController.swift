8a02471a4b89d1a975b82a1dc43d1c6465b18904
sunset12321215 <sunsetno08@gmail.com>
2020-09-03 13:59:47 +0700

    Initial Commit

---
+++ GameProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameProject
+//
+//  Created by Vu Xuan Cuong on 9/3/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
