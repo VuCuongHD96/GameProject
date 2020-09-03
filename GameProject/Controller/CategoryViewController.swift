9716c1e4d2b6c8f469e5155ce3fdc4ada4769d4f
sunset12321215 <sunsetno08@gmail.com>
2020-09-03 14:25:18 +0700

    Create base 4 screens, MVC

---
+++ GameProject/Controller/CategoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CategoryViewController.swift
+//  GameProject
+//
+//  Created by Vu Xuan Cuong on 9/3/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+final class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ GameProject/Controller/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  GameProject
+//
+//  Created by Vu Xuan Cuong on 9/3/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+final class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
