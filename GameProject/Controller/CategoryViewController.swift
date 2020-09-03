16b8fae5ec36dc5183c97877942298ea896735b7
sunset12321215 <sunsetno08@gmail.com>
2020-09-03 17:13:00 +0700

    Task [#4.1, #4.2] Category Screen: Create UI & Fetch Data

--- GameProject/Controller/CategoryViewController.swift
+++ GameProject/Controller/CategoryViewController.swift
@@ -11,22 +11,64 @@ import FirebaseDatabase
 
 final class CategoryViewController: UIViewController {
     
+    //  MARK:   - Outlet
+    @IBOutlet private weak var tableView: UITableView!
+    
     //  MARK:   - Properties
+    struct Constant {
+        static let cellID = "categoryCell"
+        static let navigationTitle = "Category"
+    }
     var ref: DatabaseReference!
+    var categoryArray = [String]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     //  MARK:   - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchData()
+        setupViews()
     }
     
     //  MARK:   - Setup Data
     private func fetchData() {
         ref = Database.database().reference()
-        ref.child("1iaMdC7zOZDlle8jkCxed-N5fegvGdNT1stzZphOcV6s")//.child("LichSu")
+        ref.child(FirebaseChild.childKey)
             .observeSingleEvent(of: .value) { (snapshot) in
                 let postDict = snapshot.value as? [String : Any] ?? [:]
-                print(postDict.keys)
+                self.categoryArray = Array(postDict.keys)
+                print(postDict)
         }
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    //  MARK:   - Setup Views
+    private func setupViews() {
+        navigationItem.title = Constant.navigationTitle
+    }
+}
+
+extension CategoryViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: Constant.cellID) else {
+            return UITableViewCell()
+        }
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category
+        return cell
+    }
+}
+
+extension CategoryViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        //  TODO:   - Navigate to Game Screen
     }
 }
