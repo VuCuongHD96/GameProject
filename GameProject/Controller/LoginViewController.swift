1a88d5dd2078746a168ae17e29650e807e32e1f2
sunset12321215 <sunsetno08@gmail.com>
2020-09-08 09:50:07 +0700

    Task [#03]: Login Screen

--- GameProject/Controller/LoginViewController.swift
+++ GameProject/Controller/LoginViewController.swift
@@ -7,11 +7,45 @@
 //
 
 import UIKit
+import GoogleSignIn
 
 final class LoginViewController: UIViewController {
 
+    // MARK: - Outlet
+    @IBOutlet private weak var googleSignInButton: GIDSignInButton!
+    
+    // MARK: - Properties
+    struct Constant {
+        static let clientID = "876601697477-1g020d6pet94m6tn6qbrcj15iu649cs2.apps.googleusercontent.com"
+        static let navigationTitle = "Login"
+    }
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupData()
+        setupViews()
+    }
+    
+    // MARK: - Setup Data
+    private func setupData() {
+        GIDSignIn.sharedInstance()?.delegate = self
+        GIDSignIn.sharedInstance()?.clientID = Constant.clientID
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+    }
+    
+    // MARK: - Setup Views
+    private func setupViews() {
+        navigationItem.title = Constant.navigationTitle
     }
 }
 
+extension LoginViewController: GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        let email = user.profile.email ?? "No Email"
+        guard let categoryScreen = storyboard?.instantiateViewController(identifier: "categoryScreen") as? CategoryViewController else {
+            return
+        }
+        navigationController?.pushViewController(categoryScreen, animated: true)
+    }
+}
