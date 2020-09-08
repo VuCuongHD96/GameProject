fb5892fe29e9e9038d7c5062a1be20d90d2638a0
cinderela95 <thang.phanminh@V000076s-Mac-mini.local>
2020-09-08 17:03:43 +0700

    score

---
+++ GameProject/Controller/ScoreTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ScoreTableViewCell.swift
+//  GameProject
+//
+//  Created by Phan Minh Thang on 9/8/20.
+//  Copyright © 2020 Vu Xuan Cuong. All rights reserved.
+//
+
+import UIKit
+
+class ScoreTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblSTT: UILabel!
+    
+    @IBOutlet weak var lblCategory: UILabel!
+    
+    @IBOutlet weak var lblScore: UILabel!
+    
+    @IBOutlet weak var lblResult: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
